4a8feb36e809d45d2e1ccf5fd16c5745a5f04ec5
Arlene Andrews <kara_arlene@yahoo.com>
2018-10-12 14:18:45 -0600

    Cards in progress ERROR INSECURE

--- spotmanager.py
+++ spotmanager.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_templaterender_template, request, redirect, url_for, session, flash, g
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,19 +10,19 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "spotdatabase.db
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-# app.secret_key = "&"
+# app.secret_key = "Fox&Dragon"
 
 db = SQLAlchemy(app)
 
 class Spot(db.Model):
     place = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-    address = db.Column(db.String(80), unique=True, nullable=False)
-    phone = db.Column(db.Int, unique=True)
-    lat = db.Column(db.Real, nullable=False)
-    lon = db.Column(db.Read, nullable=False)
+    """ address = db.Column(db.String(80), unique=True, nullable=False)
+    phone = db.Column(db.INT, unique=True)
+    lat = db.Column(db.INT, nullable=False)
+    lon = db.Column(db.INT, nullable=False)
     visited = db.Column(db.INT, unique=True, nullable=False)
     rating = db.Column(db.INT, unique=True, nullable=False)
-    tags = db.Column(db.String(80), unique=True, nullable=False)
+    tags = db.Column(db.String(80), unique=True, nullable=False) """
    
     def __repr__(self):
         return "<Name: {}>".format(self.place)
@@ -44,7 +44,7 @@ def home():
     if request.form:
         try:
             venue = Spot(place=request.form.get("venue")
-            # TODO: add stuuf here
+            # TODO: add stuff here
             )
             db.session.add(venue)
             db.session.commit()
@@ -65,20 +65,6 @@ def update():
     except Exception as e:
             print("Failed to update spot")
             print(e)
-    return redirect("/")
-
-# TODO: Add other adjustments
-@app.route("/updatea", methods=["POST"])
-def updatea():
-    try:
-            newwriter = request.form.get("newwriter")
-            oldwriter = request.form.get("oldwriter")
-            book = Book.query.filter_by(writer=oldwriter).first()
-            book.writer = newwriter
-            db.session.commit()
-    except Exception as e:
-        print("Couldn't update book author")
-        print(e)
     return redirect("/")
 
 @app.route("/delete", methods=["POST"])
