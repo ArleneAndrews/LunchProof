b3a61ba7c6a47aa4b105e0599685dfae773ef780
Arlene Andrews <kara_arlene@yahoo.com>
2018-10-22 12:37:09 -0600

    Moving files, still stuck ERRORS

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-from app import app
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g
+from functools import wraps
+from flask_sqlalchemy import SQLAlchemy
+from settings import key
 import unittest
 
+app = Flask(__name__)
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -10,6 +16,10 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    # Ensure that login shows correctly
+    def test_login_page_load(self):
+        tester = app.test_client(self)
+        self.assertTrue('Hello! Please log in.' in response.data)
 
 if __name__ == '__main__':
     unittest.main()
