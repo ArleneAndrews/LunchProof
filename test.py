086785a8f820e27b5c9c0e7213008bd6de15ab5e
Arlene Andrews <kara_arlene@yahoo.com>
2018-10-22 14:16:47 -0600

    Test working! ERRORS

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-import os
-
-from flask import Flask, render_template, request, redirect, url_for, session, flash, g
-from functools import wraps
-from flask_sqlalchemy import SQLAlchemy
-from settings import key
 import unittest
-
-app = Flask(__name__)
+from spotmanager import app
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -19,7 +12,8 @@ class FlaskTestCase(unittest.TestCase):
     # Ensure that login shows correctly
     def test_login_page_load(self):
         tester = app.test_client(self)
-        self.assertTrue('Hello! Please log in.' in response.data)
+        response = tester.get('/login')
+        self.assertTrue(b'Hello! Please log in.' in response.data) 
 
 if __name__ == '__main__':
     unittest.main()
